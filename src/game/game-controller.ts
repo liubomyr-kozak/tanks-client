89dd749f4fc85eec25ba50dfc642b6a6d91f695d
TonyTekk <thetonytekk@gmail.com>
2015-10-16 23:23:27 +0300

    del hot key

--- src/game/game-controller.ts
+++ src/game/game-controller.ts
@@ -1,63 +1,24 @@
 import {controller} from "../annotations";
 
-@controller('GameController', ['hotkeys', 'tank', '$scope'])
+@controller('GameController', ['tank', '$scope'])
 export class GameController {
-	private addHotkey:Function;
-
-	constructor(hotkeys, tank, $scope) {
-		/**
-		 * @method addHotkey
-		 *
-		 * If `upFunc` parameter is not given it will use
-		 * built in engine to pick the most suitable option automatically
-		 *
-		 * @param key Binding key
-		 * @param downFunc Callback function on key down
-		 * @param upFunc Callback function on key up
-		 */
-		this.addHotkey = (key:string, downFunc:Function, upFunc?:Function) => {
-			if (typeof downFunc === 'function' && typeof upFunc !== 'function') {
-				hotkeys.add({
-					combo: key,
-					callback: downFunc
-				});
-			} else if (typeof downFunc === 'function' && typeof upFunc === 'function') {
-				hotkeys.add({
-					combo: key,
-					action: 'keydown',
-					callback: downFunc
-				});
-				hotkeys.add({
-					combo: key,
-					action: 'keyup',
-					callback: upFunc
-				});
+	constructor(tank, $scope) {
+		$scope.onKeypress = function ($event) {
+			switch($event.keyCode) {
+				case 105:
+					tank.forwardStart();
+					break;
+				case 107:
+					tank.backwardStart();
+					break;
+				case 106:
+					tank.leftStart();
+					break;
+				case 108:
+					tank.rightStart();
+					break;
 			}
 		};
-
-		this.addHotkey('i', () => {
-			tank.forwardStart();
-		}, () => {
-			tank.forwardStop();
-		});
-
-		this.addHotkey('k', () => {
-			tank.backwardStart();
-		}, () => {
-			tank.backwardStop();
-		});
-
-		this.addHotkey('j', () => {
-			tank.leftStart();
-		}, () => {
-			tank.leftStop();
-		});
-
-		this.addHotkey('l', () => {
-			tank.rightStart();
-		}, () => {
-			tank.rightStop();
-		});
 
 		$scope.onMouseMove = function ($event) {
 			tank.updateGunAngle($event.clientX, $event.clientY);
